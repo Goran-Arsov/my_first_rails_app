b66f68fc4c84d2cfffad88a76b711510f68ba799
Goran-Arsov <arsphy@yahoo.com>
2019-01-20 22:35:56 +0100

    My second branch, changed controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
 protect_from_forgery with: :exception
 
 def hello
-    render html: "Hello worlds!"
+    render html: "¡Hello worlds!"
 end
 end
